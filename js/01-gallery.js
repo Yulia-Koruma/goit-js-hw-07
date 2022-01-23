ff3b99f3161b4cd37430ae966d67100bf2442d8b
Yulia Koruma <90381665+Yulia-Koruma@users.noreply.github.com>
2022-01-23 22:21:38 +0200

    done 1-2 task

---
+++ js/01-gallery.js
@@ -0,0 +1,59 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery')
+const imageRef = document.querySelectorAll('.gallery__image')
+
+const galleryCardsMarkup = createGalleryCardsMarkup(galleryItems)
+galleryEl.insertAdjacentHTML('beforeend', galleryCardsMarkup)
+
+// console.log(createGalleryCardsMarkup(galleryItems))
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+         <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+         </a>
+        </div >
+        `
+    })
+    .join('')
+}
+
+galleryEl.addEventListener('click', onPictureClick)
+// galleryEl.addEventListener('keydown', onEscBtnPress)
+
+function onPictureClick(e) {
+  e.preventDefault()
+  if (!e.target.classList.contains('gallery__image')) {
+    return
+  }
+
+  imageRef.src = e.target.dataset.source
+
+  const instance = basicLightbox.create(
+    `<img src="${imageRef.src}" alt="${imageRef.alt}" />`
+  )
+
+  instance.show()
+
+  if (instance.visible()) {
+    galleryEl.addEventListener('keydown', onEscBtnPress)
+  }
+
+  function onEscBtnPress(e) {
+    if (e.code === 'Escape') {
+      instance.close()
+    }
+  }
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,26 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery')
+const galleryCardsMarkup = createGalleryCardsMarkup(galleryItems)
+galleryEl.insertAdjacentHTML('beforeend', galleryCardsMarkup)
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `
+    })
+    .join('')
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+})
+console.log(lightbox)
